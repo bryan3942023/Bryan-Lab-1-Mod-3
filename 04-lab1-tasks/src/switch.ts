c0fdb6be0d94b332a50d8d696eb3cace37c7a668
bryan3942023 <126121373+bryan3942023@users.noreply.github.com>
2023-02-28 16:59:41 -0400

    Lab

---
+++ 04-lab1-tasks/src/switch.ts
@@ -0,0 +1,29 @@
+
+const dia:number=7;
+switch (dia) {
+    case 1:
+        console.log('Lunes');
+        
+        break;
+    case 2:
+        console.log('Martes');
+        break;
+    case 3:
+        console.log('Miercoles');    
+        break;
+    case 4:
+        console.log('Jueves');
+        break;
+    case 5:
+        console.log('Viernes');
+        break;
+    case 6:
+        console.log('Sabado');
+        break;
+    case 7:
+        console.log('Domingo');
+        break;    
+    default:
+        console.log('No existe ese dia de la semana');
+        break;
+}
